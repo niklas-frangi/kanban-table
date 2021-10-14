0092b5c0e50325debfddacb376f9914a37d345a2
Niklas Björk <89797537+NikBjo72@users.noreply.github.com>
2021-10-14 21:05:54 +0200

    Några justeringar i main.js så att den funkar samt funktionen som kollar om användaren redan är inloggad. Bygger på att vi gör en egen funktion som körs vid inloggning loginTrue() (se ticket i trello) samt att funktionen som ska skriva ut korten heter printExistingCard(). Dessa är just nu bortkommenterade. Kollar även så att användaren som finns i LoSt finns med i inloggningsuppgifter.json (#3)

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 //Placeholders för import av modulfiler (exempel nedan)
-import { funktionsnamn } from '.modules/filnamn.mjs';
-import { funktionsnamn } from '.modules/filnamn.mjs';
-import { funktionsnamn } from '.modules/filnamn.mjs';
-import { funktionsnamn } from '.modules/filnamn.mjs';
-import { funktionsnamn } from '.modules/filnamn.mjs';
-import { funktionsnamn } from '.modules/filnamn.mjs';
+import { checkLoginStatus } from './modules/checkLoginStaus.mjs';
+//import { funktionsnamn } from './modules/filnamn.mjs';
+//import { funktionsnamn } from './modules/filnamn.mjs';
+//import { funktionsnamn } from './modules/filnamn.mjs';
+//import { funktionsnamn } from './modules/filnamn.mjs';
+//import { funktionsnamn } from './modules/filnamn.mjs';
 
 // Global
 const global = {
-  loginForm = document.getElementById('loginform'),
-  userName = document.getElementById('username'),
-  passWord = document.getElementById('password'),
-  loginBtn = document.getElementById('loginBtn'),
-  logoutBtn = document.getElementById('logoutBtn'),
-  kanbanboardClass = document.getElementById('kanbanBoard'),
-  kanbanboardTodo = document.getElementById('Todo'),
-  kanbanboardDoing = document.getElementById('Doing'),
-  kanbanboardTest = document.getElementById('Test'),
-  kanbanboardDone = document.getElementById('Done'),
-  todoBtn = document.getElementById('todoBtn'),
-  doingBtn = document.getElementById('doingBtn'),
-  testBtn = document.getElementById('testBtn'),
-  doneBtn = document.getElementById('doneBtn'),
-  wrongCredentials = document.getElementById('wrongCredentials')
+  loginForm: document.getElementById('loginform'),
+  userName: document.getElementById('username'),
+  passWord: document.getElementById('password'),
+  loginBtn: document.getElementById('loginBtn'),
+  logoutBtn: document.getElementById('logoutBtn'),
+  kanbanboardClass: document.getElementById('kanbanBoard'),
+  kanbanboardTodo: document.getElementById('Todo'),
+  kanbanboardDoing: document.getElementById('Doing'),
+  kanbanboardTest: document.getElementById('Test'),
+  kanbanboardDone: document.getElementById('Done'),
+  todoBtn: document.getElementById('todoBtn'),
+  doingBtn: document.getElementById('doingBtn'),
+  testBtn: document.getElementById('testBtn'),
+  doneBtn: document.getElementById('doneBtn'),
+  wrongCredentials: document.getElementById('wrongCredentials')
 }
 export { global };
 
 // Cards cointainer
 let allCards = [];
 export { allCards };
+
+checkLoginStatus();
