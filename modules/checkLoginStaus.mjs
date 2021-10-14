0da57ab3fc5054b3e8f850631375e78c211b8f62
Niklas Björk <89797537+NikBjo72@users.noreply.github.com>
2021-10-14 21:45:10 +0200

    Missade att rensa modulen på testkod, så har gjort det nu. (#4)
    * Några justeringar i main.js så att den funkar samt funktionen som kollar om användaren redan är inloggad. Bygger på att vi gör en egen funktion som körs vid inloggning loginTrue() (se ticket i trello) samt att funktionen som ska skriva ut korten heter printExistingCard(). Dessa är just nu bortkommenterade. Kollar även så att användaren som finns i LoSt finns med i inloggningsuppgifter.json
    
    * Rensad kod i modulen. Missade det i första commiten

--- modules/checkLoginStaus.mjs
+++ modules/checkLoginStaus.mjs
@@ -1,21 +1,17 @@
 export function checkLoginStatus() {
 
     let localStorageCurrentUser = localStorage.getItem("currentUser");
-    console.log(localStorageCurrentUser);
+    
     const getData = async () => {
         const response = await fetch ("inloggningsuppgifter.json");
         const users = await response.json();
-        console.log(users);
+        
         let filterUsername = users.map((users) => users.username);
 
         for (let i = 0; i < filterUsername.length; ++i){
             if (localStorageCurrentUser === filterUsername[i]){
-                console.log("Logga in!");
                 // loginTrue();
                 // printExistingCard();
-            }
-            else{
-                console.log("Loggar inte in!");
             }
         }
     }
