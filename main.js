764a1f6dd932a832719970fbde583d5e175da6c2
Robin Karlsson <88037608+niborium@users.noreply.github.com>
2021-10-14 17:16:45 +0200

    Grundstruktur v2 (#2)
    * Lagt till grundstruktur med filer och inloggningsuppgifter.json
    
    * Lagt till grundstruktur med filer och inloggningsuppgifter.json
    
    * uppdaterade med export som jag hade glömt
    
    * lagt till footer-text efter mockup och lagt till en välkomst text på kanbanboard (hjälper när vi ska toggla diven)
    
    * grundstruktur v2 - uppdatering efter möte
    
    * fixed request
    
    * flyttad div

--- main.js
+++ main.js
@@ -8,15 +8,24 @@ import { funktionsnamn } from '.modules/filnamn.mjs';
 
 // Global
 const global = {
-loginForm = document.getElementById('loginform'),
-userName = document.getElementById('username'),
-passWord = document.getElementById('password'),
-loginBtn = document.getElementById('loginBtn'),
-logoutBtn = document.getElementById('logoutBtn'),
-kanbanboardClass = document.getElementById('kanbanboard'),
-kanbanboardTodo = document.getElementById('Todo'),
-kanbanboardDoing = document.getElementById('Doing'),
-kanbanboardTest = document.getElementById('Test'),
-kanbanboardDone = document.getElementById('Done')
+  loginForm = document.getElementById('loginform'),
+  userName = document.getElementById('username'),
+  passWord = document.getElementById('password'),
+  loginBtn = document.getElementById('loginBtn'),
+  logoutBtn = document.getElementById('logoutBtn'),
+  kanbanboardClass = document.getElementById('kanbanBoard'),
+  kanbanboardTodo = document.getElementById('Todo'),
+  kanbanboardDoing = document.getElementById('Doing'),
+  kanbanboardTest = document.getElementById('Test'),
+  kanbanboardDone = document.getElementById('Done'),
+  todoBtn = document.getElementById('todoBtn'),
+  doingBtn = document.getElementById('doingBtn'),
+  testBtn = document.getElementById('testBtn'),
+  doneBtn = document.getElementById('doneBtn'),
+  wrongCredentials = document.getElementById('wrongCredentials')
 }
 export { global };
+
+// Cards cointainer
+let allCards = [];
+export { allCards };
