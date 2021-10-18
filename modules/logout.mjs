a820d5cbf771a715b238f58f7a1a1f6899c30245
Niklas Frangi <frni21nj@student.ju.se>
2021-10-18 09:37:14 +0200

    importerat funtions till main och lagt till eventListener på loggutknapp

--- modules/logout.mjs
+++ modules/logout.mjs
@@ -1,6 +1,8 @@
 import { global } from "../main.js";
 import { removeCards } from './removeCards.mjs';
 
+global.logoutBtn.addEventListener("click", logout);
+
 // Tar bort currentUser från localStorage
 export function logout() {
     localStorage.removeItem("currentUser");
