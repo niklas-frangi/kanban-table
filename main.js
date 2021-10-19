3dae339468cb8c3952786e73846a0320b28dc4a2
Sofia Marhaug <sofiamarhaug@gmail.com>
2021-10-19 11:00:56 +0200

    funktionalitet för nytt kort, styles + event listeners för alla nytt kort knappar

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-//Placeholders för import av modulfiler (exempel nedan)
+//Import modules
 import { checkLoginStatus } from './modules/checkLoginStaus.mjs';
 import { loginFunction } from './modules/loginFunction.mjs';
 import { logout } from './modules/logout.mjs';
 import { removeCards } from './modules/removeCards.mjs';
-//import { funktionsnamn } from './modules/filnamn.mjs';
-//import { funktionsnamn } from './modules/filnamn.mjs';
+import { newCard } from './modules/newCard.mjs';
 
 // Global
 const global = {
@@ -38,3 +37,17 @@ checkLoginStatus();
 
 // Lägger till ett klickevent på knappen för utloggning som kör utloggningsfunktionen.
 global.logoutBtn.addEventListener("click", logout);
+
+// Event listener for new card buttons
+global.todoBtn.addEventListener("click", function() {
+  newCard("todo");
+});
+global.doingBtn.addEventListener("click", function() {
+  newCard("doing");
+});
+global.testBtn.addEventListener("click", function(){
+  newCard("test");
+});
+global.doneBtn.addEventListener("click", function(){
+  newCard("done");
+});
